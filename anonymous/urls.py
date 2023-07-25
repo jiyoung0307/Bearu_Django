cbc00f84b0dd52af68b2ffd3dc9787ff70d64b74
DESKTOP-8VD2695\dolph <jiyoungjang0307@gmail.com>
2023-07-25 19:37:01 +0900

    로그인 페이지 중간 커밋!

--- anonymous/urls.py
+++ anonymous/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from board.views import board
+from user.views import signin
 
 # 아무것도 없는 경로로 들어왔을 때 board라는 함수를 실행시켜줘
 urlpatterns = [
-    path("", board, name="board")
+    path("", board, name="board"),
+    path("user/signin", signin, name="signin")
 ]
